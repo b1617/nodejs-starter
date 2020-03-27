2cb774001784aa9f0dec4faa128804e80ed523fd
Dhinesh <sattia.dhinesh@gmail.fr>
2020-03-27 20:26:53 +0100

    http methods test for user.service

--- src/test/user.test.js
+++ src/test/user.test.js
@@ -2,9 +2,11 @@ const userService = require('../services/user.services');
 const mongoose = require('mongoose');
 const model = {
     email: "test@test.com",
-    password: "12345"
-}
-
+    firstname: "test",
+    lastname: "test",
+    age: 22,
+    password: "1344"
+};
 describe('User model test', () => {
     beforeAll(done => {
         mongoose.connect('mongodb://localhost/starter', {
@@ -13,6 +15,12 @@ describe('User model test', () => {
         });
         done()
     });
+
+    it('should create user with jwt token', async () => {
+        const object = await userService.createUser(model);
+        expect(object.user.email).toEqual(model.email);
+    });
+
     it('should get users', async () => {
         const users = await userService.getUsers();
         expect(users).toBeInstanceOf(Array);
@@ -20,8 +28,18 @@ describe('User model test', () => {
 
     it('should get user', async () => {
         const user = await userService.getUser(model.email);
-        console.log(user);
         expect(user.email).toEqual(model.email);
+    });
+
+    it('should update', async () => {
+        model.firstname = "test2";
+        const object = await userService.updateUser(model);
+        expect(object.user.firstname).toEqual(model.firstname);
+    });
+
+    it('should delete', async () => {
+        const result = await userService.deleteUser(model);
+        expect(result.deletedCount).toBe(1);
     })
 
     afterAll(done => {
