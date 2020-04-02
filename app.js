937a2c772ddf1bb5c7ba4df748b117c00a74e904
Dhinesh <sattia.dhinesh@gmail.fr>
2020-04-02 10:24:07 +0200

    add cors and prettier

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const redis = require('./src/config/database/redis');
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRouter = require('./src/routes/user.routes');
+const cors = require('cors');
 mongoose.start();
 redis.start();
 const app = express();
-
-const PORT = process.env.PORT || 5000
+app.use(cors());
+const PORT = process.env.PORT || 5000;
 // config
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,4 +19,4 @@ app.get('/', (res, req) => res.send('Hello World'));
 app.use('/user', userRouter);
 
 // start
-app.listen(PORT, () => console.log(`Server is running on port ${3000}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,16 +3,16 @@ const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    firstname: String,
-    lastname: String,
-    age: Number,
-    email: String,
-    password: String,
+  firstname: String,
+  lastname: String,
+  age: Number,
+  email: String,
+  password: String
 });
 
-UserSchema.pre('save', function (next) {
-    this.password = bcrypt.hashSync(this.password, 10);
-    next();
+UserSchema.pre('save', function(next) {
+  this.password = bcrypt.hashSync(this.password, 10);
+  next();
 });
 
 const User = mongoose.model('User', UserSchema);
